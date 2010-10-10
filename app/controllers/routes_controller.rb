c0999bc8905d29d3713df1c02dfc2b3792c4e699
Howard Yeh <hayeah@gmail.com>
2010-10-10 13:09:09 -0700

    route id massage

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,11 @@
 class RoutesController < ApplicationController
   def show
-    route = Route.where(:uid => params["id"]).first
+    id = params["id"]
+    if id.length < 3
+      id += "0" * (3-id.length)
+    end
+    id = id.downcase
+    route = Route.where(:uid => id).first
     render :text => route.to_hash.to_json
   end
 
