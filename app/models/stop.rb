88dda7196d22a44ffe627e167ec8325bc0a5e353
Howard Yeh <hayeah@gmail.com>
2010-10-10 14:09:35 -0700

    split the time

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,6 +2,10 @@ class Stop < ActiveRecord::Base
   belongs_to :route
   # stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station
   def to_hash
-    attributes
+    attributes.merge("times" => times)
+  end
+
+  def times
+    self["times"].split("$")
   end
 end
